dcf9ca84a395804ad324ca483486c000e0c12a62
mpappalardo <marco.pappalardo@amadeus.com>
2015-10-15 11:11:21 +0200

    asd

--- codinggame.py
+++ codinggame.py
@@ -52,7 +52,7 @@ class Position(object):
 players = {}
 for n in range(player_count):
     players[n] = Player(Position(0,0), n, 0)
-
+holes = {}
 
 
 def updateHistory(player_id, x, y):
@@ -68,6 +68,14 @@ def updateHistory(player_id, x, y):
   history[x][y] = player_id
 
 
+def next_move():
+    me = players[my_id]
+
+    if history[me.position.x][me.position.y + 1] != ".":
+        print "RIGHT"
+    print 'UP'
+
+
 # game loop
 while 1:
     helper_bots = int(raw_input())
@@ -78,9 +86,6 @@ while 1:
         players[i].position.x = x
         players[i].position.y = y
 
-    for player in players:
-        debug(player)
-
     removal_count = int(raw_input())
     for i in xrange(removal_count):
         remove_x, remove_y = [int(j) for j in raw_input().split()]
@@ -90,4 +95,4 @@ while 1:
 
     # print >> sys.stderr, "history: ", history
     print_history()
-    print "up"
+    next_move()
