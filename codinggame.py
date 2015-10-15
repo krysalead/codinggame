c81e0a2f96d993ecedc4fabf303a94bb1b063c99
odaumas <odaumas@amadeus.com>
2015-10-15 09:21:50 +0200

    init

---
+++ codinggame.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+player_count = int(raw_input())
+my_id = int(raw_input())
+
+# game loop
+while 1:
+    helper_bots = int(raw_input())
+    for i in xrange(player_count):
+        x, y = [int(j) for j in raw_input().split()]
+    removal_count = int(raw_input())
+    for i in xrange(removal_count):
+        remove_x, remove_y = [int(j) for j in raw_input().split()]
+
+    # Write an action using print
+    # To debug: print >> sys.stderr, "Debug messages..."
+
+    print "up"
