5ad9b239899e93db3e53030e265940b4c4d5da25
mkalinowski <michal.kalinowski@amadeus.com>
2015-10-15 10:24:23 +0200

    improves history

--- codinggame.py
+++ codinggame.py
@@ -16,30 +16,21 @@ my_id = int(raw_input())
 # . 0 . . 3 . . .
 # . . B B + B B 1
 
-class player(object):
-	def __init__(self,position,id,missile):
-		self.position = position
-		self.id = id
-		self.missile = missile
-	def updatePosition(self,position):
-		self.position=position
-		
-class position(object):
-	def __init__(self,x,y):
-		self.x=x
-		self.y=y
-		
- history = [["." for x in range(15)] for x in range(30)]
-players = {0 : "A", 1: "B", 2: "C", 3: "D"}
+history = [["." for x in range(15)] for x in range(30)]
+playerTrail = {0 : "A", 1: "B", 2: "C", 3: "D"}
 
 def updateHistory(player, x, y):
+  print >> sys.stderr, player 
   # 1 replace all heads with trails
-  for XXX in history:
-    XXX = [players[player] for xxx in XXX if xxx == player]
+  for xx, row in enumerate(history):
+    for yy, cell in enumerate(row):
+      #print >> sys.stderr, "CELL", cell
+      if cell == player: # if cell is player head
+        #print >> sys.stderr, cell, player
+        history[xx][yy] = playerTrail[player] # update with player trail
+  
   # 2 add new head
-  history[x][y] = player # 2 add new head
   history[x][y] = player
-
 
 
 # game loop
@@ -56,6 +47,6 @@ while 1:
 
     # Write an action using print
     # To debug: print >> sys.stderr, "Debug messages..."
-
-    print >> sys.stderr, "history: ", history
-    print "up"
+    print >> sys.stderr, "history:", history
+    
+    print "UP"
